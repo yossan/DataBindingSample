2143ee1834a53b4c404b3f825e93693d07337d3a
Kosuke Yoshimoto <kosuke.yoshimoto@gmail.com>
2021-06-06 13:32:51 +0900

    Add the following samples: * View references * Event handling

--- app/src/main/java/io/github/yossan/databindingsample/MainActivity.kt
+++ app/src/main/java/io/github/yossan/databindingsample/MainActivity.kt
@@ -2,10 +2,28 @@ package io.github.yossan.databindingsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import io.github.yossan.databindingsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.resultHandler = ResultListener(binding)
+    }
+
+    class ResultListener(val binding: ActivityMainBinding) : View.OnClickListener {
+        override fun onClick(v: View) {
+            binding.resultTextView.text = when(binding.ageEditText.text.toString().toInt()) {
+                in 0..20 -> "NG"
+                in 21..130 ->"OK"
+                else -> "It's not human."
+            }
+        }
     }
 }
+
